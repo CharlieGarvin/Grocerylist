446bd4142a255cefdd915ba098035b435ed05fe9
a-diazrod <150281101+a-diazrod@users.noreply.github.com>
2023-11-27 11:24:09 -0700

    Update remove items.java

--- GroceryList/src/remove items.java
+++ GroceryList/src/remove items.java
@@ -1,18 +1,30 @@
- System.out.print("Choose an item to remove (0 to exit): ");
-                int itemChoice = scanner.nextInt();
+ System.out.println("Which items would you like to remove?");
+                    System.out.println("You have:");
+                    int i = 1;
+               for (String item : list.userCart.keySet()) {
+                        int count = itemCount.get(item);
+                        if (count > 0) {
+                            System.out.println(i + ". " + item + " (" + count + ")");
+                            i++;
+                        }
+                    }
+                    System.out.println("0. Exit");
+                    System.out.print("User: ");
+                    int removeItemOption = scanner.nextInt();
+                    if (removeItemOption == 0) {
+                        break;
+                    }
 
-                if (itemChoice == 0) {
-                    continue;
-                }
-
-                if (itemChoice > 0 && itemChoice <= groceryBill.groceryList.size()) {
-                    GroceryItems selectedItem = groceryBill.groceryList.get(itemChoice - 1);
-
+                    String itemToRemove = (String) itemCount.keySet().toArray()[removeItemOption - 1];
                     System.out.print("How many would you like to remove? ");
-                    int quantityToRemove = scanner.nextInt();
+                    int removeQuantity = scanner.nextInt();
+                    int currentQuantity = itemCount.get(itemToRemove);
 
-                    selectedItem.removeQuantity(quantityToRemove);
-                    groceryBill.removeItem(selectedItem);
-                } else {
-                    System.out.println("Invalid choice. Please choose a valid option.");
-                }
+                    if (removeQuantity > currentQuantity) {
+                        System.out.println("You don't have that many " + itemToRemove + " to remove.");
+                    } else {
+                        itemCount.put(itemToRemove, currentQuantity - removeQuantity);
+                        totalCost -= items.get(itemToRemove) * removeQuantity;
+                        System.out.println("You have " + itemToRemove + " (" + (currentQuantity - removeQuantity) + ") left");
+                    }
+                    break;
